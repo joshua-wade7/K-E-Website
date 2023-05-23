2ff7d71514c738b5ae63d21fea0177c8ceb2d032
joshua-wade7 <joshua.nelson0124@gmail.com>
2023-05-23 14:19:22 -0600

    updated directory structure to better match best practices.  Created package.json at root level and added corresponding scripts.

--- server.js
+++
@@ -1,7 +0,0 @@
-// Require in express to
-
-// setup what port it will be running on
-
-// Setup Middleware
-
-// Setup connection to server
---
+++ server/server.js
@@ -0,0 +1,11 @@
+// Require in express to
+const express = require("express");
+const { ApolloSever } = require("apollo-server-express");
+const path = require("path");
+const { authMiddleware } = require("./utils/auth");
+
+// setup what port it will be running on
+
+// Setup Middleware
+
+// Setup connection to server
