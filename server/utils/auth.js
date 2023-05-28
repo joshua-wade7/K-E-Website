3012314d22ae87ba6e5eba5404dd1daccc7496b0
joshua-wade7 <joshua.nelson0124@gmail.com>
2023-05-27 21:15:52 -0600

    added secret to auth.js and working on authorization middleware.

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,1 +1,18 @@
 const jwt = require("jsonwebtoken");
+
+const secret = "mysecretssssshhhhhhhh";
+const expiration = "6h";
+
+module.exports = {
+  authMiddleware: function ({ req }) {
+    let token = req.body.token || req.query.token || req.headers.authroization;
+
+    if (req.headers.authorization) {
+      token = token.split(" ").pop().trim();
+    }
+
+    if (!token) {
+      return req;
+    }
+  },
+};
