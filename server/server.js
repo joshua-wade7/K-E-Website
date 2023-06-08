0592c271441564be8e2ab1df565f57e2365682f4
joshua-wade7 <joshua.nelson0124@gmail.com>
2023-06-08 15:37:09 -0600

    created schemas, config directories and corresponding files within each.

--- server/server.js
+++ server/server.js
@@ -4,6 +4,9 @@ const { ApolloSever } = require("apollo-server-express");
 const path = require("path");
 const { authMiddleware } = require("./utils/auth");
 
+// require in schemas (typedefs and resolvers)\
+const { typeDefs, resolvers } = require("./schemas");
+
 // setup what port it will be running on
 
 // Setup Middleware
